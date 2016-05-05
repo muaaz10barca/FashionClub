7aa536458f099e1606e94cfc8e082b371bff9053
Muaaz <muaaz10barca@gmail.com>
2016-05-05 14:46:00 +0800

    finishing navigation drawer

--- app/src/main/java/muaz_limkok/fashionclub/MainActivity.java
+++ app/src/main/java/muaz_limkok/fashionclub/MainActivity.java
@@ -2,24 +2,68 @@ package muaz_limkok.fashionclub;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // lets start
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView)findViewById(R.id.shitstuff);
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                // when user clicks close the drawer
+                drawerLayout.closeDrawers();
+
+                if (item.getItemId() == R.id.map) {
+                    Toast.makeText(MainActivity.this,"map",Toast.LENGTH_SHORT).show();
+                }
+
+                if (item.getItemId() == R.id.info) {
+                    Toast.makeText(MainActivity.this,"in",Toast.LENGTH_SHORT).show();
+                }
+
+                if (item.getItemId() == R.id.about) {
+                    Toast.makeText(MainActivity.this,"ab",Toast.LENGTH_SHORT).show();
+                }
+
+                // rest of the menu
+
+                return false;
+            }
+        });
+
+        // setup drawer toggle
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout, toolbar,R.string.app_name,
+                R.string.app_name);
+
+        drawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
     }
 
     @Override
@@ -51,4 +96,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
