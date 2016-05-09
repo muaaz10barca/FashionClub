f5a40cd95524bac2c3759db5a08d68d7c213c17f
Islam <ibouhaha67@gmail.com>
2016-05-09 20:55:08 +0800

    finishing information activity and connecting with youtupe api

--- app/src/main/java/muaz_limkok/fashionclub/Information.java
+++ app/src/main/java/muaz_limkok/fashionclub/Information.java
@@ -1,13 +1,49 @@
 package muaz_limkok.fashionclub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Toast;
 
-public class Information extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class Information extends YouTubeFailureRecoveryActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inform_layout);
+        YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize("AIzaSyDn2jiaTh6VP6NHor2wa3gVRyGVNNMOd70", this);
+    }
+
+
+    @Override
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView) findViewById(R.id.youtube_view);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        if (!b) {
+            youTubePlayer.cueVideo("wKJ9KzGQq0w");
+        }
+    }
+
+    public void sendEmail(View view) {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"muaaz10barca@gmail.com"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "More Information about the products");
+        i.putExtra(Intent.EXTRA_TEXT   , "Hello Fashion Club");
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(Information.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
